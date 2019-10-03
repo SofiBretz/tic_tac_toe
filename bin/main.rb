ebae9eb29465d51554fd298c4cc83ecd4bdf06a4
thsvr <vr.ths.zd@gmail.com>
2019-10-03 19:32:44 -0300

    add welcome message to players

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,14 @@
-puts "Hello world!"
+# Welcome to players
+
+puts "Welcome Players!"
+puts "Tic Tac Toe is a game for two players."
+puts "The game is played on a grid that's 3 squares by 3 squares"
+puts "If you are X, your friend (or the computer in this case) is O"
+puts "Players take turns putting their marks in empty squares"
+puts "The first player to get 3 of her/his/they marks in a row (up, down, across, or diagonally) is the winner"
+puts "When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie"
+puts "Good Luck!"
+
+# Users input
+
 
