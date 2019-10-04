c614c1b205b22c8b614f33ccb7b783a342da424d
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-04 13:29:41 -0300

    welcome, players_info and display methods defined

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 # Welcome to players
-
+def welcome
 puts "Welcome Players!"
 puts "Tic Tac Toe is a game for 2 players."
 puts "The game is played on a grid that's 3 x 3"
@@ -8,9 +8,10 @@ puts "Players take turns putting their marks in empty squares"
 puts "The first player to get 3 marks in a row WIN"
 puts "When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie"
 puts "Good Luck! <_<"
+end
 
 # Users input
-
+def players_info
 puts "Welcome to Tic-Tac-Toe"
 puts "Are you ready?"
 
@@ -25,3 +26,21 @@ second_player = gets.chomp.capitalize!
     puts "#{second_player} for you is X"
 
 puts "Let the game begin!"
+display
+end
+
+def display
+    puts "\n       #{@board[0]} | #{@board[1]} | #{@board[2]} "
+
+    puts '       ---------- '
+
+    puts "       #{@board[3]} | #{@board[4]} | #{@board[5]} "
+
+    puts '       ---------- '
+
+    puts "       #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  
+
+
