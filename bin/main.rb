1b4de5c98484884181907df3ee69e5fdabba321d
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-07 18:25:09 -0300

    added file game_logic to bin folder, starting the game process

--- bin/main.rb
+++ bin/main.rb
@@ -37,15 +37,11 @@ class Players
 end
 
 class Board
-    def display
+    def board
         puts "\n     #{@board[0]} | #{@board[1]} | #{@board[2]} "
-
         puts '       ---------- '
-
         puts "       #{@board[3]} | #{@board[4]} | #{@board[5]} "
-
         puts '       ---------- '
-
         puts "       #{@board[6]} | #{@board[7]} | #{@board[8]} "
     end
 
