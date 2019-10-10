8afb4ec4570dd61a791493b0ab21df7f917ccdb2
thsvr <vr.ths.zd@gmail.com>
2019-10-10 16:29:30 -0300

    add board test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,9 +6,5 @@ RSpec.describe Board do
     it "shows cell interface" do
         expect(board.cell).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     end
-    it "Return default value of counter = 0" do
-        expect(board.counter).to eql(0)
-    end
 end
-
-end #end line 1
+end 
