bf33d44db9db2c7526be0d2f24d283abdd62d4e4
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-10 17:32:18 -0300

    added 2 more examples when the element has been choosen, all tests passed

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -16,4 +16,24 @@ RSpec.describe Player do
       expect(player.element).to eql(nil)
     end
   end
-end # last end
+
+  describe '#element.choosen' do
+    it 'Returns the element that the player created, in this case x' do
+      player.element = 'x'
+      expect(player.element).to eql('x')
+    end
+  end
+
+  describe '#element.choosen' do
+    it 'Returns the element that the player created, in this case o' do
+      player.element = 'o'
+      expect(player.element).to eql('o')
+    end
+  end
+
+  describe '#score' do
+    it 'returns the score of the players' do
+      expect(player.score).to eql(0)
+    end
+  end
+end
