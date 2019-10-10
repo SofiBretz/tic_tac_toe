61e38c980349aa31d779792b292d3e3ad6a89437
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-10 16:53:35 -0300

    added test on move method, all examples passed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe Board do
         expect(board.cell).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     end
 end
-end 
+
+    describe "#move" do
+    it "returns false on win_game when moved element is on a different cell that is supposed to be" do
+        expect(board.move('o', 4)).to eql([false, 1, [[1, 2, 3], ['o', 5, 6], [7, 8, 9]]])
+    end
+end
+    describe "#move" do
+    it "returns true on win_game when moved element is on a correct cell that is supposed to be" do
+        board_win = Board.new([[1, 'o', 'o'], [4, 5, 6], [7, 8, 9]])
+        expect(board_win.move('o', 1)).to eql([true, 1, [%w[o o o], [4, 5, 6], [7, 8, 9]]])
+    end
+end
+
+
+
+
+end #last end
