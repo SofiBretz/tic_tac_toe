696268d61a0d81ddde1f9fead77decb09d344831
thsvr <vr.ths.zd@gmail.com>
2019-10-10 18:09:56 -0300

    add tests to horizont possibilities to win

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/board.rb'
 
 RSpec.describe Board do
   let(:board) { Board.new }
+  
   describe '#Board' do
     it 'shows cell interface' do
       expect(board.cell).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,35 @@ require_relative '../lib/game.rb'
 RSpec.describe Game do
     let(:cell) {[[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
     let(:result) { Game.new(cell)}
+
     describe "#result.position" do
     it "returns the result of the position of the element in the board" do
      expect(result.cell).to eql(cell)
     end
+  end
+
+  describe "possible positon to win on the first row" do
+    let(:horizontal) { [["x", "x", "x"], [4, 5, 6], [7, 8, 9]] }
+    let(:result1) { Game.new(horizontal) }
+    it 'returns true when the first line has 3 equal symbols on the horizontal' do
+      expect(result1.win_game).to eql(true)
+    end  
+  end
+
+  describe "possible positon to win on the second row" do
+    let(:horizontal) { [[1, 2, 3], ["x", "x", "x"], [7, 8, 9]] }
+    let(:result1) { Game.new(horizontal) }
+    it 'returns true when the second line has 3 equal symbols on the horizontal' do
+      expect(result1.win_game).to eql(true)
+    end  
+  end
+
+  describe "possible positon to win on the third row" do
+    let(:horizontal) { [[1, 2, 3], [4, 5, 6], ["x", "x", "x"]] }
+    let(:result1) { Game.new(horizontal) }
+    it 'returns true when the third line has 3 equal symbols on the horizontal' do
+      expect(result1.win_game).to eql(true)
+    end  
   end
 
 
