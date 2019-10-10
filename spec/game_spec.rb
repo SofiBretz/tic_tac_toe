41670cd9f5569306fa351fd6b3a45b92ecc8cd18
thsvr <vr.ths.zd@gmail.com>
2019-10-10 18:35:01 -0300

    add tests to diagonal possibilities

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,49 +14,65 @@ RSpec.describe Game do
 
   describe 'possible positon to win on the first row' do
     let(:horizontal) { [%w[x x x], [4, 5, 6], [7, 8, 9]] }
-    let(:result1) { Game.new(horizontal) }
+    let(:result_h) { Game.new(horizontal) }
     it 'returns true when the first row has 3 equal symbols on the horizontal' do
-      expect(result1.win_game).to eql(true)
+      expect(result_h.win_game).to eql(true)
     end
   end
 
   describe 'possible positon to win on the second row' do
     let(:horizontal) { [[1, 2, 3], %w[x x x], [7, 8, 9]] }
-    let(:result1) { Game.new(horizontal) }
+    let(:result_h) { Game.new(horizontal) }
     it 'returns true when the second row has 3 equal symbols on the horizontal' do
-      expect(result1.win_game).to eql(true)
+      expect(result_h.win_game).to eql(true)
     end
   end
 
   describe 'possible positon to win on the third row' do
     let(:horizontal) { [[1, 2, 3], [4, 5, 6], %w[x x x]] }
-    let(:result1) { Game.new(horizontal) }
+    let(:result_h) { Game.new(horizontal) }
     it 'returns true when the third row has 3 equal symbols on the horizontal' do
-      expect(result1.win_game).to eql(true)
+      expect(result_h.win_game).to eql(true)
     end
   end
 
   describe 'possible positon to win on the first column' do
     let(:vertical) { [['O', 2, 3], ['O', 5, 6], ['O', 8, 9]] }
-    let(:result2) { Game.new(vertical) }
-    it 'returns true when the first row has 3 equal symbols on the vertical' do
-      expect(result2.win_game).to eql(true)
+    let(:result_v) { Game.new(vertical) }
+    it 'returns true when the first column has 3 equal symbols on the vertical' do
+      expect(result_v.win_game).to eql(true)
     end
   end
 
   describe 'possible positon to win on the second column' do
     let(:vertical) { [[1, 'O', 3], [4, 'O', 6], [7, 'O', 9]] }
-    let(:result2) { Game.new(vertical) }
-    it 'returns true when the first row has 3 equal symbols on the vertical' do
-      expect(result2.win_game).to eql(true)
+    let(:result_v) { Game.new(vertical) }
+    it 'returns true when the second column has 3 equal symbols on the vertical' do
+      expect(result_v.win_game).to eql(true)
     end
   end
 
   describe 'possible positon to win on the third column' do
     let(:vertical) { [[1, 2, 'O'], [4, 5, 'O'], [7, 8, 'O']] }
-    let(:result2) { Game.new(vertical) }
-    it 'returns true when the first row has 3 equal symbols on the vertical' do
-      expect(result2.win_game).to eql(true)
+    let(:result_v) { Game.new(vertical) }
+    it 'returns true when the third column has 3 equal symbols on the vertical' do
+      expect(result_v.win_game).to eql(true)
+    end
+  end
+
+  describe 'possible positon to win on the left-right diagonal' do
+    let(:diagonal) { [['X', 2, 3], [4, 'X', 6], [7, 8, 'X']] }
+    let(:result_d) { Game.new(diagonal) }
+    it 'returns true when all the 3 symbols on the left-right diagonal match' do
+      expect(result_d.win_game).to eql(true)
+    end
+  end
+
+  describe 'possible positon to win on the right-left diagonal' do
+    let(:diagonal) { [[1, 2, 'X'], [4, 'X', 6], ['X', 8, 9]] }
+    let(:result_d) { Game.new(diagonal) }
+    it 'returns true when all the 3 symbols on the right-left diagonal match' do
+      expect(result_d.win_game).to eql(true)
     end
   end
 end # end line
