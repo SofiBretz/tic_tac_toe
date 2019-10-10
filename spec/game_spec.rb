f69c1fbc2619f998ea747e371cbb8a1dfc950a1a
thsvr <vr.ths.zd@gmail.com>
2019-10-10 18:11:36 -0300

    rubocop to tests to horizont possibilities to win

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,61 +1,38 @@
+# frozen_string_literal: true
+
 require_relative '../lib/game.rb'
 
 RSpec.describe Game do
-    let(:cell) {[[1, 2, 3], [4, 5, 6], [7, 8, 9]]}
-    let(:result) { Game.new(cell)}
+  let(:cell) { [[1, 2, 3], [4, 5, 6], [7, 8, 9]] }
+  let(:result) { Game.new(cell) }
 
-    describe "#result.position" do
-    it "returns the result of the position of the element in the board" do
-     expect(result.cell).to eql(cell)
+  describe '#result.position' do
+    it 'returns the result of the position of the element in the board' do
+      expect(result.cell).to eql(cell)
     end
   end
 
-  describe "possible positon to win on the first row" do
-    let(:horizontal) { [["x", "x", "x"], [4, 5, 6], [7, 8, 9]] }
+  describe 'possible positon to win on the first row' do
+    let(:horizontal) { [%w[x x x], [4, 5, 6], [7, 8, 9]] }
     let(:result1) { Game.new(horizontal) }
     it 'returns true when the first row has 3 equal symbols on the horizontal' do
       expect(result1.win_game).to eql(true)
-    end  
+    end
   end
 
-  describe "possible positon to win on the second row" do
-    let(:horizontal) { [[1, 2, 3], ["x", "x", "x"], [7, 8, 9]] }
+  describe 'possible positon to win on the second row' do
+    let(:horizontal) { [[1, 2, 3], %w[x x x], [7, 8, 9]] }
     let(:result1) { Game.new(horizontal) }
     it 'returns true when the second row has 3 equal symbols on the horizontal' do
       expect(result1.win_game).to eql(true)
-    end  
+    end
   end
 
-  describe "possible positon to win on the third row" do
-    let(:horizontal) { [[1, 2, 3], [4, 5, 6], ["x", "x", "x"]] }
+  describe 'possible positon to win on the third row' do
+    let(:horizontal) { [[1, 2, 3], [4, 5, 6], %w[x x x]] }
     let(:result1) { Game.new(horizontal) }
     it 'returns true when the third row has 3 equal symbols on the horizontal' do
       expect(result1.win_game).to eql(true)
-    end  
+    end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-end #end line
+end # end line
