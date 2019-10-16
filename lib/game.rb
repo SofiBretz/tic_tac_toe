1414c047c71f2675416eaa945f1fa471318337c6
thsvr <vr.ths.zd@gmail.com>
2019-10-16 12:27:26 -0300

    replace some win possibilities methods to just one method

--- lib/game.rb
+++ lib/game.rb
@@ -14,46 +14,19 @@ class Game
   private
 
   def diagonal
-    left_diagonal || right_diagonal
-  end
-
-  def left_diagonal
-    @cell[0][0] == @cell[1][1] && @cell[1][1] == @cell[2][2]
-  end
-
-  def right_diagonal
+    @cell[0][0] == @cell[1][1] && @cell[1][1] == @cell[2][2] ||
     @cell[2][0] == @cell[1][1] && @cell[1][1] == @cell[0][2]
   end
 
   def vertical
-    vertical_one || vertical_two || vertical_three
-  end
-
-  def vertical_one
-    @cell[0][0] == @cell[1][0] && @cell[1][0] == @cell[2][0]
-  end
-
-  def vertical_two
-    @cell[0][1] == @cell[1][1] && @cell[1][1] == @cell[2][1]
-  end
-
-  def vertical_three
+    @cell[0][0] == @cell[1][0] && @cell[1][0] == @cell[2][0] ||
+    @cell[0][1] == @cell[1][1] && @cell[1][1] == @cell[2][1] ||
     @cell[0][2] == @cell[1][2] && @cell[1][2] == @cell[2][2]
   end
 
   def horizontal
-    horizontal_one || horizontal_two || horizontal_three
-  end
-
-  def horizontal_one
-    @cell[0][0] == @cell[0][1] && @cell[0][1] == @cell[0][2]
-  end
-
-  def horizontal_two
-    @cell[1][0] == @cell[1][1] && @cell[1][1] == @cell[1][2]
-  end
-
-  def horizontal_three
+    @cell[0][0] == @cell[0][1] && @cell[0][1] == @cell[0][2] ||
+    @cell[1][0] == @cell[1][1] && @cell[1][1] == @cell[1][2] ||
     @cell[2][0] == @cell[2][1] && @cell[2][1] == @cell[2][2]
   end
 end
