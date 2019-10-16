deb315144c7a0997bbe98c546967bae0917d76a8
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-10-16 12:35:52 -0300

    added short methods on Draw Class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
 class Game
-  attr_writer :cell
+  attr_accessor :cell
+
+  def initialize(cell)
+    @cell = cell
+  end
+
+  def win_game
+    diagonal || vertical || horizontal
+  end
+
+  private
+
+  def diagonal
+    @cell[0][0] == @cell[1][1] && @cell[1][1] == @cell[2][2] ||
+    @cell[2][0] == @cell[1][1] && @cell[1][1] == @cell[0][2]
+  end
+
+  def vertical
+    @cell[0][0] == @cell[1][0] && @cell[1][0] == @cell[2][0] ||
+    @cell[0][1] == @cell[1][1] && @cell[1][1] == @cell[2][1] ||
+    @cell[0][2] == @cell[1][2] && @cell[1][2] == @cell[2][2]
+  end
+
+  def horizontal
+    @cell[0][0] == @cell[0][1] && @cell[0][1] == @cell[0][2] ||
+    @cell[1][0] == @cell[1][1] && @cell[1][1] == @cell[1][2] ||
+    @cell[2][0] == @cell[2][1] && @cell[2][1] == @cell[2][2]
+  end
+end
+
+class Draw
+  attr_accessor :cell
 
   def initialize(cell)
     @cell = cell
